5cdf920d350c6eeb1a9f99703b753127bb8e84ba
Mauricio916 <wcarrasco@tecnologicoismac.edu.ec>
2023-11-17 09:58:46 -0500

    "V0003 bla"

--- src/com/distribuida/principal/Principal.java
+++ src/com/distribuida/principal/Principal.java
@@ -2,4 +2,12 @@ package com.distribuida.principal;
 
 public class Principal {
 
+	public static void main(String[] args) {
+		
+		System.out.println("hol mundo desde git!!...");
+		System.out.println("hol mundo desde git!!...");
+		System.out.println("hol mundo desde git!!...");
+		
+	}
+	
 }
